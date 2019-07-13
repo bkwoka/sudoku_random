d32f9634d8710bbbca07c2c3fe4e5f8681316dc5
bkwoka <bkwoka1@gmail.com>
2019-07-13 21:34:10 +0000

    Extend function check in line()

--- main.py
+++ main.py
@@ -1,27 +1,13 @@
-import random
-
-table = [[0] * 9] * 9
-
-numbers = [num for num in range(1, 10)]
-
-def shuffle():
-
-    shuff_numbers = numbers[:]
-    random.shuffle(shuff_numbers)
-
-    return shuff_numbers
+from uui import *
 
 
-table[0] = shuffle()
-table[8] = shuffle()
 
-for i in range(0, 9, 3):
-    print("")
-    for j in range(0, 9, 3):
-        print("{} {} {}  {} {} {}  {} {} {}". format(
+def start():
+    table[0] = shuffle()
+    table[8] = shuffle()
+    make_table()
+    print_table()
 
-            table[i + 0][j], table[i + 0][j + 1], table[i + 0][j + 2],
-            table[i + 1][j], table[i + 1][j + 1], table[i + 1][j + 2],
-            table[i + 2][j], table[i + 2][j + 1], table[i + 2][j + 2],
-        ))
 
+if __name__ == "__main__":
+    start()
--- uui.py
+++ uui.py
@@ -1,6 +1,25 @@
+import random
 
-def check_in_line():
-    pass
+table = [[0] * 9] * 9
+numbers = [num for num in range(1, 10)]
+
+
+def shuffle():
+
+    shuffle_numbers = numbers[:]
+    random.shuffle(shuffle_numbers)
+
+    return shuffle_numbers
+
+
+def check_in_line(num_block, number):
+    if num_block in range(0, 3):
+        block_to_compare = list(range(0, 3))
+        block_to_compare.remove(num_block)
+
+
+
+
 
 
 def check_in_column():
@@ -8,3 +27,23 @@ def check_in_column():
 
 def check_in_block():
     pass
+
+
+def make_table():
+    list_of_num = shuffle()
+    list_blocks_for_make = [2]
+    while list_of_num:
+        for index, number in enumerate(list_of_num):
+            check_in_line(list_blocks_for_make[0], list_of_num[index])
+
+
+def print_table():
+    for i in range(0, 9, 3):
+        print("")
+        for j in range(0, 9, 3):
+            print("{} {} {}  {} {} {}  {} {} {}".format(
+
+                table[i + 0][j], table[i + 0][j + 1], table[i + 0][j + 2],
+                table[i + 1][j], table[i + 1][j + 1], table[i + 1][j + 2],
+                table[i + 2][j], table[i + 2][j + 1], table[i + 2][j + 2],
+            ))
